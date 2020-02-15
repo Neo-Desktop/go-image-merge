00ca6a453eab7de96b6ce95c977e268e0be1ebfe
Neo Anderson <neo@neo.yt>
2020-02-15 13:52:24 -0800

    rescope to package, add kitten, use gim to test bytes

--- cmd/gim/gim.go
+++ cmd/gim/gim.go
@@ -1,30 +1,42 @@
 package main
 
 import (
-	gim "github.com/ozankasikci/go-image-merge"
+	"bytes"
 	"image/color"
 	"image/jpeg"
 	"log"
 	"os"
+	gim "github.com/Neo-Desktop/go-image-merge"
 )
 
 func main() {
-	grids := []*gim.Grid{
-		{
-			ImageFilePath:   "./cmd/gim/input/ginger.png",
-			BackgroundColor: color.White,
-		},
-		{
-			ImageFilePath:   "./cmd/gim/input/ginger.png",
-			BackgroundColor: color.RGBA{R: 0x8b, G: 0xd0, B: 0xc6},
-		},
-	}
-	rgba, err := gim.New(grids, 2, 1).Merge()
+
+	imgFile, err := os.Open("./192.jfif")
 	if err != nil {
 		log.Panicf(err.Error())
 	}
 
-	file, err := os.Create("cmd/gim/output/merged.jpg")
+    buf := new(bytes.Buffer)
+	buf.ReadFrom(imgFile)
+	image := buf.Bytes()
+
+	grids := []*gim.Grid{
+		{
+			ImageBytes:   image,
+			ImageType:    "jfif",
+			BackgroundColor: color.White,
+		},
+		{
+			ImageFilePath:   "./192.jfif",
+			BackgroundColor: color.RGBA{R: 0x8b, G: 0xd0, B: 0xc6},
+		},
+	}
+	rgba, err := gim.New(grids, 1, 2).Merge()
+	if err != nil {
+		log.Panicf(err.Error())
+	}
+
+	file, err := os.Create("merged.jpg")
 	if err != nil {
 		log.Panicf(err.Error())
 	}
